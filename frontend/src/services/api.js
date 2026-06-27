898fe170f1ffbb3ba1c1a5144978846da9a94024
swatisomya <swatisomya218@gmail.com>
2026-06-27 22:48:08 +0530

    Update requirements and api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://rankingproject.onrender.com";
 
 export const loginUser = async (email, password) => {
   const res = await fetch(`${BASE_URL}/login`, {
